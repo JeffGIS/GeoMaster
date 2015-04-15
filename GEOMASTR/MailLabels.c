93b52f44ce1fca86185e086df1ffa275b169daa3
Jeff Smith <jeff@thmgis.com>
2015-04-15 16:16:08 -0500

    First production $MAILLABELS

--- GEOMASTR/MailLabels.c
+++ GEOMASTR/MailLabels.c
@@ -1,7 +1,9 @@
 #include "graphint.h"   
 #include "gmextern.h"
 
-static void DrawStuff(HDC hdc);
+// format fits Avery 5160 labels
+
+static void DrawStuff(HDC hdc, HDIB32 hDIB32, int x, int y, int width, int height, char lines[4][256],BOOL wantImage);
 static void InitDocStruct(DOCINFO* di, char* docname);
 static BOOL CALLBACK AbortProcML(HDC hDC, int Error);
 static HDC GetPrinterDC(void);
@@ -10,19 +12,19 @@ static BOOL displayImage(HDC hDC, HDIB32 hDIB32, int x, int y,int width, int hei
 {
 	BOOL rtn = FALSE;
 	RECT rect;
+	int border = height * 0.05;
 	if (hDIB32)
 	{
 		BITMAPINFOHEADER DibInfo;
 
 		GetBitmapInfoFromHandle(&DibInfo, hDIB32);
 		rect.left = x;
-		rect.top = y;
+		rect.top = y+border;
 		rect.right = rect.left + width;
-		rect.bottom = rect.top + height;
-		rtn = DisplayBMInRect32(hDC, hDIB32, rect, TRUE);
-		DestroyDIB32(hDIB32, FALSE);
+		rect.bottom = rect.top + height - border;
+		rtn = DisplayBMInRect32(hDC, hDIB32, rect, FALSE);
 	}
-
+	return rtn;
 }
 /*===============================*/
 /* The Abort Procudure           */
@@ -44,9 +46,10 @@ static BOOL CALLBACK AbortProcML(HDC hDC, int Error)
 BOOL PrintMailLabels(int nArgs, LPSTR *Arg, LPSTR OutLoc)
 {
 	HWND hWndParent = hWndMain;
-	BOOL rtn = TRUE;
+	BOOL rtn = FALSE;
 	HDC        hDC;
 	DOCINFO    di;
+	HANDLE	hDB = 0;
 	char ImagePath[MAX_PATH]="[%DL]icons\\maillabel.bmp";
 
 	// Need a printer DC to print to.
@@ -55,18 +58,36 @@ BOOL PrintMailLabels(int nArgs, LPSTR *Arg, LPSTR OutLoc)
 	// Did you get a good DC?
 	if (!hDC)
 	{
-		MessageBox(NULL, "Error creating DC", "Error",
-			MB_APPLMODAL | MB_OK);
+//		MessageBox(NULL, "Error creating DC", "Error",
+//			MB_APPLMODAL | MB_OK);
 		return FALSE;
 	}
-	else
+	else if (OpenDataFile(Arg[1], "", BT_READ, &hDB))
 	{
 		int pageHeight = GetDeviceCaps(hDC, VERTRES);
+		int pageHeightmm = GetDeviceCaps(hDC, VERTSIZE);
 		int pageWidth = GetDeviceCaps(hDC, HORZRES);
-		int height = pageHeight / 10;
-		int width = pageWidth / 3;
-
+		int pageWidthmm = GetDeviceCaps(hDC, HORZSIZE);
+		double pageWidthInch = 0.0393701 * pageWidthmm;
+		double pageHeightInch = 0.0393701 * pageHeightmm;
+		double pixPerInchH = pageHeight / pageHeightInch;
+		double pixPerInchW = pageWidth / pageWidthInch;
+		int physWidth = GetDeviceCaps(hDC, PHYSICALWIDTH);
+		int physHeight = GetDeviceCaps(hDC, PHYSICALHEIGHT);
+		int physOffsetX = GetDeviceCaps(hDC, PHYSICALOFFSETX);
+		int physOffsetY = GetDeviceCaps(hDC, PHYSICALOFFSETY);
+		double xInch = 0.25, yInch = 0.5;
+		int x = xInch * pixPerInchW - physOffsetX;
+		int y = yInch * pixPerInchH - physOffsetY;
+		int startY = y;
+		int height = (pageHeight - y*2) / 10;
+		int width = pixPerInchW * 2.72;// (pageWidth - x * 2) / 3;
+		HFONT hFont, OldFont;
+		int PointSize = 8;
+		int FontSize = -MulDiv(PointSize, GetDeviceCaps(hDC, LOGPIXELSY), 72);
+		int nLabels = NumSQLRows(hDB);
 		HDIB32 hDIB32 = LoadDIB32(ImagePath, FALSE);
+		char lines[4][256];
 
 		// You always have to use an AbortProc().
 		if (SetAbortProc(hDC, AbortProcML) == SP_ERROR)
@@ -82,15 +103,50 @@ BOOL PrintMailLabels(int nArgs, LPSTR *Arg, LPSTR OutLoc)
 
 		// Print one page.
 		StartPage(hDC);
-		DrawStuff(hDC);
-		EndPage(hDC);
+		hFont = CreateFont(FontSize, 0, 0, 0, FW_NORMAL,0, 0, 0, 0, 0, 0, 0, 0, "Arial");
 
+		OldFont = SelectObject(hDC, hFont);
+		//SetTextColor(hDC, RGB(128, 0, 0));
+		SetTextColor(hDC, 0);
+		while (nLabels > 0)
+		{
+			y = startY;
+			for (int irow = 0; irow < 10; irow++)
+			{
+				for (int icol = 0; icol < 3; icol++)
+				{
+					if (!nLabels--)
+					{
+						//EndPage(hDC);
+						goto Exit;
+					}
+					FetchDBRec(hDB);
+					for (int line = 0; line < 4; line++)
+					{
+						sprintf(lines[line], "[LABELLINE%i]", line + 1);
+						ExpandText(lines[line]);
+					}
+					DrawStuff(hDC, hDIB32, x + (icol*width), y, width, height,lines,FALSE);
+				}
+				y += height;
+			}
+			if (nLabels)
+				EndPage(hDC);
+		}
+
+Exit:
 		// Indicate end of document.
 		EndDoc(hDC);
 
 		// Clean up
+		SelectObject(hDC, OldFont);
+		DeleteObject(hFont);
 		DeleteDC(hDC);
+		DestroyDIB32(hDIB32, FALSE);
+		CloseDataFile(TRUE, &hDB);
+		rtn = TRUE;
 	}
+
 	return rtn;
 }
 
@@ -108,10 +164,12 @@ static HDC GetPrinterDC(void)
 	pdlg.Flags = PD_RETURNDC;
 
 	// Invoke the printer dialog box.
-	PrintDlg(&pdlg);
-	// hDC member of the PRINTDLG structure contains
-	// the printer DC.
-	return pdlg.hDC;
+	if (PrintDlg(&pdlg))
+		// hDC member of the PRINTDLG structure contains
+		// the printer DC.
+		return pdlg.hDC;
+	else
+		return 0;
 }
 
 
@@ -130,9 +188,51 @@ void InitDocStruct(DOCINFO* di, char* docname)
 /*===============================*/
 /* Drawing on the DC             */
 /* ==============================*/
-static void DrawStuff(HDC hdc)
+static void DrawStuff(HDC hdc, HDIB32 hDIB32, int x, int y, int width, int height,char lines[4][256],BOOL wantImage)
 {
-	// This is the function that does draws on a given DC.
-	// You are printing text here.
-	TextOut(hdc, 0, 0, "Test Printing", lstrlen("Test Printing"));
+	char str[1024];
+	int w = width*(0.7 / 2.72);
+	static int n = 1;
+	POINT p[2];
+	SIZE siz;
+
+	p[0].y = p[1].y = y;
+	p[0].x = x;
+	p[1].x = x + width;
+	//Polyline(hdc, p, 2);
+	p[0].y = y;
+	p[1].y = y + height;
+	p[0].x = x + w;
+	p[1].x = x + w;
+	//Polyline(hdc, p, 2);
+
+
+	SetTextAlign(hdc, TA_LEFT | TA_TOP);
+	sprintf(str, "Name%i", n);
+	if (wantImage)
+		displayImage(hdc, hDIB32, x, y, w, height);
+	else
+		w = w / 5;
+	for (int line = 0; line < 4; line++)
+	{
+		if (*lines[line])
+		{
+			TextOut(hdc, x + w, y + height / 4, lines[line], lstrlen(lines[line]));
+			GetTextExtentPoint32(hdc, lines[line], lstrlen(lines[line]), &siz);
+			y += siz.cy;
+		}
+	}
+	/*
+	TextOut(hdc, x + w, y + height / 4, str, lstrlen(str));
+	GetTextExtentPoint32(hdc, str, lstrlen(str), &siz);
+	y += siz.cy;
+	sprintf(str, "Address%i", n);
+	TextOut(hdc, x + w, y + height / 4, str, lstrlen(str));
+	GetTextExtentPoint32(hdc, str, lstrlen(str), &siz);
+	y += siz.cy;
+	sprintf(str, "City State Sip Code%i", n++);
+	TextOut(hdc, x + w, y + height / 4, str, lstrlen(str));
+	GetTextExtentPoint32(hdc, str, lstrlen(str), &siz);
+	y += siz.cy;*/
+
 }
