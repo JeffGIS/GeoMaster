8530e2b8d38caba79946835def12752623d0a47c
Jeff Smith <jeff@thmgis.com>
2016-12-21 10:32:30 -0600

    Add midpoint to GMDInternal calculations

--- GEOMASTR/graphic4.c
+++ GEOMASTR/graphic4.c
@@ -4557,11 +4557,16 @@ BOOL DisplayPointItem (HDC hDC,POINT WinPoint,double size, double rot, int Symbo
 	int		ii; 
 	double	height=0, width=0;
 	static	BOOL	fast=FALSE;
+	static BOOL first = TRUE;
 
 	if (fast)
 	{
+		if (first)SaveDC(hDC);
+		first = FALSE;
+		SetDisplayMode(hDC, GF_TEXTMODE);
 		SetPixel (hDC,WinPoint.x,WinPoint.y,0);
-{
+		//RestoreDC (hDC,-1);
+	{
 #if ENABLETRACE
 GSSiExitProg (705);
 #endif
--- SHR/VARSTUF2.C
+++ SHR/VARSTUF2.C
@@ -131,6 +131,7 @@ GetPolylineEP:
 					if (HiPrecis)
 					{   
 						HPDPOINT	lpPoints=lpDCurPoints;
+						
 						long		nPnts=nCurPoints;
 						
 						if (hDynamicSeg)
@@ -145,7 +146,14 @@ GetPolylineEP:
 						Type1.BPY = lpPoints->y;  
 						Type1.EPX = lpDEndPoint->x;
 						Type1.EPY = lpDEndPoint->y;
-						Type1.AZM = getazd (lpPoints,lpDEndPoint);  
+						Type1.AZM = getazd (lpPoints,lpDEndPoint); 
+						if (CurrentType == GF_LINE)
+						{
+							double MPAZ;
+							DPOINT	MidPoint = PointAtDistOnPoly(lpPoints, nPnts, Dist / 2, &MPAZ, 0);
+							Type1.MPX = MidPoint.x;
+							Type1.MPY = MidPoint.y;
+						}
 						if (CurrentType == GF_CURVE)
 						{   
 							DPOINT	BP,RP; 
@@ -191,8 +199,20 @@ GetPolylineEP:
 				}  
 				else if (CurrentProcessedPickedItem >=0) 
 				{   
-					CurLength = PickList[CurrentProcessedPickedItem].Length;
+					HANDLE hPnts;
+					int nPnts;
 					Type1.Length = ConvertDist(PickList[CurrentProcessedPickedItem].Length,OutDistUnits); 
+					if (GetPolyPoints((LPPICKDATAHEADER)&PickList[CurrentProcessedPickedItem], FALSE, &nPnts, &hPnts))
+					{
+						HPDPOINT lpPoints = (HPDPOINT)GlobalLock(hPnts);
+						double MPAZ;
+						DPOINT	MidPoint = PointAtDistOnPoly(lpPoints, nPnts, PickList[CurrentProcessedPickedItem].Length / 2, &MPAZ, 0);
+						Type1.MPX = MidPoint.x;
+						Type1.MPY = MidPoint.y;
+						GSSiGlobUlFree(&hPnts);
+					}
+					CurLength = PickList[CurrentProcessedPickedItem].Length;
+
 				}
 			}
 				break;
