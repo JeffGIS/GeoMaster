5fcc14e88f130a055d876be01ba1e21d8ee62a30
Jeff Smith <jeff@thmgis.com>
2017-07-20 12:06:38 -0500

    Allow .slt files in GMEDIT

--- GMEdit/GMEdit/GMEdit.c
+++ GMEdit/GMEdit/GMEdit.c
@@ -1134,8 +1134,8 @@ void AutoInsert (HWND hWnd,HANDLE hFile,HMENU *phMenu)
 		else
 			pBeg = testFile;
 
-
-		if (!strncmp(pBeg, "ODBC|", 5) || FileType(pBeg) == 1 || strstr (pBeg,".GDB"))
+		_strupr(testFile);
+		if (!strncmp(pBeg, "ODBC|", 5) || FileType(pBeg) == 1 || strstr(pBeg, ".GDB") || strstr(pBeg, ".SLT"))
 		{
 			HANDLE hDB=0;
 			int itype = OpenDataFile (testFile,"",OF_READ,&hDB);
